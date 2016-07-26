39ce9f1c02312a617552dd510411418b50a1d156
ryang <ryang@DESKTOP-AEBMSQJ>
2016-07-25 23:41:48 -0400

    Add project files.

---
+++ cSharp ConsoleCalc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cScC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num01;
+            int num02;
+
+            Console.Write("Type a number to be multiplied: ");
+            num01 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Type another number: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Result is: " + num01 * num02);
+            Console.ReadKey();
+        }
+    }
+}
