a012beed6077f9dc5e7e5a88a12adb157409a8dd
Ryan Woodcock <ryanguy426@gmail.com>
2016-07-25 23:54:04 -0400

    Comment code for easier reference

--- cSharp ConsoleCalc/Program.cs
+++ cSharp ConsoleCalc/Program.cs
@@ -4,21 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace cScC
+namespace cScC // This namespace looks like I had a stroke while naming it...
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Store numbers the user enters for later use
             int num01;
             int num02;
 
-            Console.Write("Type a number to be multiplied: ");
-            num01 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Type another number: ");
-            num02 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Result is: " + num01 * num02);
-            Console.ReadKey();
+            // FIXME: Only supports multiplication
+            Console.Write("Type a number to be multiplied: "); // Inform user they should type a number
+            num01 = Convert.ToInt32(Console.ReadLine()); // Read entered number and store it in num01, and convert it to int32 since console expects a string.
+            Console.Write("Type another number: "); // Inform user they should type another number
+            num02 = Convert.ToInt32(Console.ReadLine()); // Read entered number and store it in num02, and convert it to int32 since console expects a string.
+            Console.WriteLine("Result is: " + num01 * num02); // Inform user of the sum of their numbers by multiplying num01 by num02 and writing the result to the console.
+            Console.ReadKey(); // Wait for user input before closing the console window
         }
     }
 }
